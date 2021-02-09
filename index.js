2fd439d507ef70fb97e852d6bae0e9ae7f0b1fa1
Dio Audia <76801729+dioaudr@users.noreply.github.com>
2021-02-09 10:24:53 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// Insert your code here
+var pi = Math.PI;
+var jariJari;
+
+// Algoritma 
+/** Jika diketahui pi sama dengan 3.14 dan jari2 sama dengan 14
+ * maka nilai keliling lingkaran sama dengan 2 diakli pi dikali jari2 */
+
+//Pseudocode 
+/** STORE kelilinglingkaran WITH k 
+ *  STORE konstanta WITH pi
+ *  STORE jari2 WITH r
+ * 
+ *  IF pi EQUALS TO 3.14 
+ *     r  EQUALS TO 14
+ * ELSE IF k EQUALS TO 2 x pi x r
+ * DISPLAY k
+ * 
+ * END IF */
